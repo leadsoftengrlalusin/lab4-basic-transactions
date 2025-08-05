a6f2dabdd83908147d6ee0a6d8f01b193046e351
leadsoftengrlalusin <johnrovic.lalusin@gcash.com>
2025-08-05 09:54:56 +0800

    git init

---
+++ src/main/groovy/com/example/transactionlab/TransactionlabApplication.groovy
@@ -0,0 +1,13 @@
+package com.example.transactionlab
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class TransactionlabApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(TransactionlabApplication, args)
+	}
+
+}
